554f9c3c0751ea3361b994140e30e4e3f527df84
Oliver Huang <greenohlbur@gmail.com>
2021-11-23 17:43:47 -0800

    Adding all PSoC Files

---
+++ science wow.cydsn/main.c
@@ -0,0 +1,56 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+//#include "project.h"
+#include "CANLibrary.h"
+#include "CANScience.h"
+#include <stdint.h>
+
+#define SERVO_SET_ID 0x0D
+#define SENSOR_PULL_ID 0xF5
+
+int main(void)
+{
+    //CyGlobalIntEnable; /* Enable global interrupts. */
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    InitCAN(DEVICE_GROUP_SCIENCE, 0x1); 
+    //SCP_1_Start();
+    for(;;)
+    {
+        /* Place your application code here. */
+        CANPacket* current; 
+        
+        int poll = PollAndReceiveCANPacket(current); 
+        if (poll == 0) {
+            int ID = GetPacketID(current);
+            switch (ID) {
+                case SERVO_SET_ID : //servo set 
+                    {    
+                        uint8_t servoID = GetScienceServoIDFromPacket(current);
+                        uint8_t angle = GetScienceServoAngleFromPacket(current);
+                        //send this stuff to i2c
+                    }
+                    break;
+                case SENSOR_PULL_ID : //sensor pull
+                    //packet will only have 1 piece of data so just use dot operator, no function exists
+                    {
+                        uint8_t sensor_type = DecodeTelemetryType(current);
+                        //fetch sensor data with ADC method call and sensor read
+                    }
+                    break;
+                
+            }
+            
+        }
+    }
+}
+
+/* [] END OF FILE */
